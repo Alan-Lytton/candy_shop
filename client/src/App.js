74bed1c1157e5634cfa72e6b3ef371a4b83f56a6
Christopher Meehan <chrismeehan91@gmail.com>
2023-04-18 12:27:46 -0500

    added navbar component and css folder

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
 import LandingPage from './components/LandingPage';
+import Navbar from './components/Navbar';
 function App() {
   return (
     <div className="App">
+    <Navbar/>
     <LandingPage/>
     </div>
   );
---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import axios from 'axios'
+import {Link} from 'react-router-dom'
+import '../css/navbar.css'
+
+const Navbar = () => {
+    return (
+        <div className="navbar">
+            <div>Candy Shop NavBar!!</div>
+        </div>
+    )
+}
+
+export default Navbar
