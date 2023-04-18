a5adaff6c324acfb6b1f1ef02a48660ffaf3d5e4
Chris Bridgewater <cgbridgewater@outlook.com>
2023-04-18 00:15:23 -0700

    update server file
    added user route link, cookie parser, dotenv, credentials, changed mongoose.config to match candy.config and added .js   candy.route commented out for now

--- server/server.js
+++ server/server.js
@@ -1,14 +1,17 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
 const app = express();
 const port = 8000;
 
-app.use(cors());
+require("dotenv").config();
+
+app.use(cookieParser());
+app.use(cors({credentials:true, origin: 'http://localhost:3000'}))
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
-require("./config/mongoose.config");
-require("./routes/candy.route")(app);
-
-
+require("./config/candy.config.js");
+// require("./routes/candy.route")(app);     // commented out until something is attached, app wont run!!
+require("./routes/user.route")(app);
 
 app.listen(port,() => console.log(`listening on port: ${port}`));
