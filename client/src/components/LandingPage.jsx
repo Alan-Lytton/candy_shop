ae5692fa420e5bc346f8227c0ced66077cf16a03
Jsingh651 <108104100+Jsingh651@users.noreply.github.com>
2023-04-18 19:02:46 -0700

    updated landing page

--- client/src/components/LandingPage.jsx
+++ client/src/components/LandingPage.jsx
@@ -3,9 +3,16 @@ import "../css/landingPage.css"
 const LandingPage = () => {
   return (
     <div>
-    <h1 className='containerLink'>I'll take you to the candy shop
-    One taste of what we got
-    We'll have you spending all you got</h1>
+    <div className="all_item-container">
+    <div className='containerLink'>
+    <h1 className='title-link'> I'll take you to the candy shop.</h1>
+    <h1 className='title-link'> One taste of what we got.</h1>
+    <h1 className='title-link'> We'll have you spending all you got.</h1>
+    </div>
+    <div className="action-button">
+    <button class="submit">Shop Candies</button>
+    </div>
+    </div>
     </div>
   )
 }
