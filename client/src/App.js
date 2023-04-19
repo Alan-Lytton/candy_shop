c0d797fca733a79a05a1582afc51060716108340
Christopher Meehan <chrismeehan91@gmail.com>
2023-04-18 23:14:18 -0500

    added create candy form. altered candy controller. added app.js browserrouter

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,18 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LandingPage from './components/LandingPage';
+import CreateCandy from './components/CreateCandy';
 
 
 function App() {
   return (
     <div className="App">
-     
-      <LandingPage/>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<LandingPage/>}/>
+          <Route path='/admin/candy/create' element={<CreateCandy/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
