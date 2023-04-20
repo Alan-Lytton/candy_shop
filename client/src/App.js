c6538393bf6d77db9baff38b881187be5c032790
Jsingh651 <108104100+Jsingh651@users.noreply.github.com>
2023-04-19 21:31:47 -0700

    Updated app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LandingPage from './components/LandingPage';
-import CreateCandy from './components/CreateCandy';
 import AboutUs from './components/AboutUs';
-import UserForm from "./components/UserForm";
+import OneCandy from './components/OneCandy';
+import AllCandies from './components/AllCandies';
 import UserLogin from "./components/UserLogin";
+import UserForm from "./components/UserForm";
+import CreateCandy from './components/CreateCandy';
+import EditCandy from './components/EditCandy';
+
 
 function App() {
   return (
@@ -13,10 +17,10 @@ function App() {
         <Routes>
           <Route path='/' element={<LandingPage/>}/>
           <Route path='/about/us' element={<AboutUs/>}/>
-          <Route path='/admin/register' element={<UserForm/>}/>
-          <Route path='/admin/login' element={<UserLogin/>}/>
-          <Route path='/all/candies' element={<AllCandies/>}/>
           <Route path='/one/candy' element={<OneCandy/>}/>
+          <Route path='/all/candies' element={<AllCandies/>}/>
+          <Route path='/admin/login' element={<UserLogin/>}/>
+          <Route path='/admin/register' element={<UserForm/>}/>
           <Route path='/admin/candy/create' element={<CreateCandy/>}/>
           <Route path='/admin/candy/edit/:id' element={<EditCandy/>}/>
         </Routes>
