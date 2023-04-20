69348a008ef6c946de9387ab3de7c595e6842035
Alan Lytton <alan.lytton.coding@gmail.com>
2023-04-20 11:11:17 -0600

    Updated for connection to MongoDB Atlas and Secure Keys

--- server/config/candy.config.js
+++ server/config/candy.config.js
@@ -1,5 +1,21 @@
 const mongoose = require('mongoose');
-mongoose.connect("mongodb://127.0.0.1:27017/candy-shop", {
+
+const dbUserName = process.env.dbUserName;
+const dbPassword = process.env.dbPassword;
+const dbCluster = process.env.dbCluster;
+
+// commented section below is for local instance of MongoDB
+
+// mongoose.connect("mongodb://127.0.0.1:27017/candy-shop", {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+// })
+//     .then(() => console.log("Established a connection to the database"))
+//     .catch(err => console.log("Something went wrong when connecting to the database", err));
+
+
+// code block below is for connection to MongoDB Atlas cloud cluster
+mongoose.connect(`mongodb+srv://${dbUserName}:${dbPassword}@${dbCluster}.mongodb.net/candy-shop?retryWrites=true&w=majority`, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 })
--- server/server.js
+++ server/server.js
@@ -3,15 +3,17 @@ const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const app = express();
 const port = 8000;
+const dotenv = require("dotenv");
 
-require("dotenv").config();
 
+dotenv.config({path:'.env'});
+dotenv.config({path:'mongoVar.env'});
 app.use(cookieParser());
 app.use(cors({credentials:true, origin: 'http://localhost:3000'}))
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 require("./config/candy.config.js");
-require("./routes/candy.route")(app);     // commented out until something is attached, app wont run!!
+require("./routes/candy.route")(app);
 require("./routes/user.route")(app);
 
 app.listen(port,() => console.log(`listening on port: ${port}`));
