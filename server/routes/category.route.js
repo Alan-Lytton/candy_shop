e4bd8c20c321aa38333138d457c70f2d155ffba8
Chris Bridgewater <cgbridgewater@outlook.com>
2023-04-23 02:13:04 -0700

    adding delete category route
    who needs gluten free anyways... it's outta here!

--- server/routes/category.route.js
+++ server/routes/category.route.js
@@ -6,4 +6,5 @@ module.exports=(app) =>{
     app.get("/api/category", CategoryController.getAllCategories)
     app.post("/api/category/add", CategoryController.createCategory)
     app.get("/api/category/:id", CategoryController.getOneCategory)
+    // app.delete("/api/category/:id", CategoryController.deleteCategory)
 }
