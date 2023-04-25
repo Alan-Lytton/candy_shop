2fdd5d1fd0c9dee0189326dc00efc4ce7eb3d7f5
Chris Bridgewater <cgbridgewater@outlook.com>
2023-04-25 14:37:00 -0700

    add admin routes

--- server/routes/candy.route.js
+++ server/routes/candy.route.js
@@ -3,11 +3,12 @@ const {authenticate} = require("../config/jwt.config");
 
 
 module.exports = (app) =>{
+    app.get("/api/admin/candy",authenticate, CandyController.getAllCandy) 
     app.get("/api/candy", CandyController.getAllCandy)
     app.get("/api/candy/:id", CandyController.getOneCandy)
+    app.get("/api/admin/candy/:id",authenticate, CandyController.getOneCandy)
     app.post("/api/candy/add", CandyController.createCandy)
-    app.patch("/api/candy/edit/:id", CandyController.updateCandy)
-    app.delete("/api/candy/delete/:id",CandyController.deleteCandy)
+    app.patch("/api/candy/edit/:id",  CandyController.updateCandy)
+    app.delete("/api/candy/delete/:id", CandyController.deleteCandy)
     app.put("/api/candy/:id/updateStock", CandyController.updateCandyStock);
-
 }
