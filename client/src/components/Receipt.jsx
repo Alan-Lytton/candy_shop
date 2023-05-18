2f43597d4bf9e2bf39185364cee43ba18b4c7c47
Chris Bridgewater <cgbridgewater@outlook.com>
2023-05-18 13:36:53 -0700

    added a empty receipt to show when no data is available

--- client/src/components/Receipt.jsx
+++ client/src/components/Receipt.jsx
@@ -15,13 +15,10 @@ const Receipt = (props) => {
     
     const [Ka_ching] = useSound(cash);
     const { details } = props;
-    const name = details.payer.name;
-    const amount = details.purchase_units[0].amount;
-    const address = details.purchase_units[0].shipping.address;
-    const order_id = details.id;
-    const items = details.purchase_units[0].items;
 
     return (
+        <div> 
+            { details.length !==0  ? 
             <div onLoad={Ka_ching()} className="wrapper">
                 <Navbar/>
                 <div className="close_container">
@@ -35,14 +32,13 @@ const Receipt = (props) => {
                         {/* left upper section */}
                         <div className="left">
                             <img className='logo_img' require src={CompanyLogo} alt="Candy Shop" />
-                            {/* <h4><span>&nbsp;&nbsp;</span></h4> */}
-                            <h5 className="push_left">Invoice#: {order_id}</h5>
+                            <h5 className="push_left">Invoice#: {details.id}</h5>
                         </div>
                         {/* right upper section */}
                         <div className="right">
                             <h4 className="push_left title">Ship To:</h4>
-                            <h5 className="push_left">{name.given_name} {name.surname}</h5>
-                            <h5 className="push_left"> {address.address_line_1}, { address.admin_area_2}, {address.admin_area_1}, {address.postal_code} {address.country_code}</h5>
+                            <h5 className="push_left">{details.payer.name.given_name} {details.payer.name.surname}</h5>
+                            <h5 className="push_left"> {details.purchase_units[0].shipping.address.address_line_1}, { details.purchase_units[0].shipping.address.admin_area_2}, {details.purchase_units[0].shipping.address.admin_area_1}, {details.purchase_units[0].shipping.address.postal_code} {details.purchase_units[0].shipping.address.country_code}</h5>
                             <h5 className="push_left"></h5>
                         </div>
                     </div>
@@ -60,8 +56,8 @@ const Receipt = (props) => {
                             <tbody>
                             {/* map through items */}
                             {
-                                items.map((item, index) =>{
-                                    return(
+                                details.purchase_units[0].items.map((item, index) =>{
+                                return(
                                 <tr key ={index}>
                                     <td className="receipt_td">{item.name}</td>
                                     <td className="receipt_td">{item.unit_amount.value}</td>
@@ -80,7 +76,7 @@ const Receipt = (props) => {
                                     <td className="receipt_td"></td>
                                     <td className="receipt_td"></td>
                                     <td className="receipt_td_ST">Sub Total:</td>
-                                    <td className="receipt_td_ST">$ {amount.value} </td>
+                                    <td className="receipt_td_ST">$ {details.purchase_units[0].amount.value} </td>
                                 </tr>
                                 <tr>
                                     <td className="receipt_td"></td>
@@ -92,7 +88,7 @@ const Receipt = (props) => {
                                     <td className="receipt_td"></td>
                                     <td className="receipt_td"></td>
                                     <td className="receipt_td">Total:</td>
-                                    <td className="receipt_td"><h3>${amount.value}</h3> </td>
+                                    <td className="receipt_td"><h3>${details.purchase_units[0].amount.value}</h3> </td>
                                 </tr>
                             </tbody>
                         </table>
@@ -113,6 +109,75 @@ const Receipt = (props) => {
                 </div>
                 <Footer/>
             </div>
+
+                :
+            
+                // empty receipt //
+            <div>
+                <div>
+                    <Navbar/>
+                    <div className="close_container">
+                        <a className="link" href="/"><button className="close">Home Page</button></a>
+                    </div>
+                    {/* recepit container */}
+                    <div className="receipt_container">
+                        <h1>RECEIPT</h1>
+                        <hr className="spacer"/>
+                        <div className="flex">
+                            {/* left upper section */}
+                            <div className="left">
+                                <img className='logo_img' require src={CompanyLogo} alt="Candy Shop" />
+                                <h5 className="push_left">Invoice#: N/A</h5>
+                            </div>
+                            {/* right upper section */}
+                            <div className="right">
+                                <h4 className="push_left title">Ship To:</h4>
+                                <h5 className="push_left">N/A</h5>
+                                <h5 className="push_left">N/A</h5>
+                                <h5 className="push_left"></h5>
+                            </div>
+                        </div>
+                        {/* items table*/}
+                        <div className="receipt_table">
+                            <table className="r_table">
+                                <thead>
+                                    <tr className="receipt_row">
+                                        <th className="receipt_th_1">Item Desc.</th>
+                                        <th className="receipt_th_2">Qty</th>
+                                        <th className="receipt_th_3">Each</th>
+                                        <th className="receipt_th_4">Total</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td className="receipt_td"> N/A</td>
+                                        <td className="receipt_td"> <span>&nbsp;&nbsp;</span></td>
+                                        <td className="receipt_td"> <span>&nbsp;&nbsp;</span></td>
+                                        <td className="receipt_td"> $0.00</td>
+                                    </tr>
+                                    <tr>
+                                        <td className="receipt_td"></td>
+                                        <td className="receipt_td"></td>
+                                        <td className="receipt_td">Shipping:</td>
+                                        <td className="receipt_td">$0.00</td>
+                                    </tr>
+                                    <tr>
+                                        <td className="receipt_td"></td>
+                                        <td className="receipt_td"></td>
+                                        <td className="receipt_td">Total:</td>
+                                        <td className="receipt_td"><h3>$0.00</h3> </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                        <h2 className="thanks" >Thanks for shopping with The Candy Shop!</h2>
+                    </div>
+
+                    <Footer/>
+                </div>
+            </div>
+        }
+        </div>
     );
 }
 
